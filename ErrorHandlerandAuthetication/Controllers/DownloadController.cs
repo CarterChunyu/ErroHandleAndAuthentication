7c80e2c87ec402b80b4a27a8e4042602129ca6db
Chunyu <qapo741@gmail.com>
2024-07-30 18:21:54 +0800

    暫存

---
+++ ErrorHandlerandAuthetication/Controllers/DownloadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorHandlerandAuthetication.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
